33cbf93d1e8ee9a1d136a2cec6c72e9d387176ea
Amujo Martha Erute <amujomartha639@gmail.com>
2026-09-08 10:03:15 +0300

    complete population assignment

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -4,5 +4,34 @@
 // One death every 13 seconds
 // One new immigrant every 45 seconds
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
+#include <iostream>
+using namespace std;
 
+int main(){
+
+int initial_population =  312032486;
+int births, deaths, immigration, seconds_per_year;
+// the births, deaths, and immigrations per year
+seconds_per_year = 365*24*60*60;
+births = seconds_per_year/7;
+deaths = seconds_per_year/13;
+immigration = seconds_per_year/45;
+
+//population for each year
+/*int year_1 = initial_population + 1*(births - deaths + immigration);
+int year_2 = initial_population + 2*(births - deaths + immigration);
+int year_3 = initial_population + 3*(births - deaths + immigration);
+int year_4 = initial_population + 4*(births - deaths + immigration);
+int year_5 = initial_population + 5*(births - deaths + immigration);*/
+
+int population_change = 0;
+
+for(int i=1;i<6;i++){
+population_change = population_change+(births - deaths + immigration);
+cout<<"population after " << i <<" years is "<< (initial_population+population_change)<<"\n"; 
+
+}
+
+    return 0;
+}
 
